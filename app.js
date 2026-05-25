14ac6b4697434b0ff204c909c23b232a08a9bef0
dayangel99-bit <day.angel.99@gmail.com>
2026-05-25 15:59:44 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,87 +1,42 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
 
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
+console.log("THREE IMPORTADO");
 
 const scene = new THREE.Scene();
-console.log("APP INICIADA");
 
-scene.background = null;
+scene.background = new THREE.Color(0xff0000);
 
 const camera = new THREE.PerspectiveCamera(
-  45,
+  75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 
-camera.position.set(0, 1, 6);
-
-const renderer = new THREE.WebGLRenderer({
-  antialias: true
-});
+const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-renderer.setPixelRatio(window.devicePixelRatio);
-
 document.body.appendChild(renderer.domElement);
 
-console.log("RENDER OK");
+const geometry = new THREE.BoxGeometry();
 
-const hemiLight = new THREE.HemisphereLight(
-  0xffffff,
-  0x444444,
-  3
-);
+const material = new THREE.MeshBasicMaterial({
+  color: 0x00ff00
+});
 
-scene.add(hemiLight);
+const cube = new THREE.Mesh(geometry, material);
 
-const dirLight = new THREE.DirectionalLight(
-  0xffffff,
-  3
-);
+scene.add(cube);
 
-dirLight.position.set(5, 10, 7);
-
-scene.add(dirLight);
-
-const loader = new GLTFLoader();
-
-loader.load(
-
-  'models/8RA_UPS_RSMAT_demo.glb',
-
-  function (gltf) {
-
-    const model = gltf.scene;
-
-    model.scale.set(2, 2, 2);
-
-    model.position.set(0, -1, 0);
-
-    scene.add(model);
-
-    console.log("MODELO CARGADO");
-
-  },
-
-  function (xhr) {
-
-    console.log((xhr.loaded / xhr.total * 100) + '% cargado');
-
-  },
-
-  function (error) {
-
-    console.error('ERROR GLB:', error);
-
-  }
-
-);
+camera.position.z = 5;
 
 function animate() {
 
   requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 
@@ -89,12 +44,4 @@ function animate() {
 
 animate();
 
-window.addEventListener('resize', () => {
-
-  camera.aspect = window.innerWidth / window.innerHeight;
-
-  camera.updateProjectionMatrix();
-
-  renderer.setSize(window.innerWidth, window.innerHeight);
-
-});
+console.log("ESCENA FUNCIONANDO");
