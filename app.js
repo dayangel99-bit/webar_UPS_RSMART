b96aa597b5f64454edd23111c36b672ed22aa00a
dayangel99-bit <day.angel.99@gmail.com>
2026-05-25 17:27:00 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,88 @@
-document.body.style.background = "red";
+import * as THREE from 'three';
 
-console.log("JS FUNCIONANDO");
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+camera.position.set(0, 1, 5);
+
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  alpha: true
+});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const hemiLight = new THREE.HemisphereLight(
+  0xffffff,
+  0x444444,
+  3
+);
+
+scene.add(hemiLight);
+
+const dirLight = new THREE.DirectionalLight(
+  0xffffff,
+  3
+);
+
+dirLight.position.set(5, 10, 7);
+
+scene.add(dirLight);
+
+const loader = new GLTFLoader();
+
+loader.load(
+  './models/8RA_UPS_RSMAT_demo.glb',
+
+  function (gltf) {
+
+    const model = gltf.scene;
+
+    model.scale.set(1.5, 1.5, 1.5);
+
+    model.position.set(0, -1, 0);
+
+    scene.add(model);
+
+    console.log("MODELO CARGADO");
+
+  },
+
+  undefined,
+
+  function (error) {
+
+    console.error(error);
+
+  }
+);
+
+function animate() {
+
+  requestAnimationFrame(animate);
+
+  renderer.render(scene, camera);
+
+}
+
+animate();
+
+window.addEventListener('resize', () => {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+});
