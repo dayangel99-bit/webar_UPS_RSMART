44a0395e9a803098854e10a4124ec596bb7dd69f
dayangel99-bit <day.angel.99@gmail.com>
2026-05-25 18:20:31 -0600

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-camera.position.set(0, 1, 5);
+camera.position.set(0, 0, 8);
 
 const renderer = new THREE.WebGLRenderer({
   antialias: true,
@@ -48,9 +48,8 @@ loader.load(
 
     const model = gltf.scene;
 
-    model.scale.set(1.5, 1.5, 1.5);
-
-    model.position.set(0, -1, 0);
+    model.scale.set(4, 4, 4);
+    model.position.set(0, -3, 0);
 
     scene.add(model);
 
